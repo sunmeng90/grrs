92bb1dc310c691ada079721e0a47781b670b5a04
Meng <sunmeng90@126.com>
2020-05-05 23:15:16 +0800

    refactory and add unit test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::fmt::Write;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -9,18 +10,9 @@ use structopt::StructOpt;
 
 fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
-    let f = File::open(args.path).with_context(|_| format!("could not open file"))?;
-    let reader = BufReader::new(f);
-
-    for line in reader.lines() {
-        match line {
-            Ok(text) if text.starts_with(&args.pattern) => {
-                println!("{}", text);
-            }
-            Ok(_) => (), // match line that not having the given pattern
-            Err(_) => (),
-        }
-    }
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
     Ok(())
 }
 
